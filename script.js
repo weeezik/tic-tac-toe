dbe43bb988711d7392167e3ae9a6fcea63bb878f
weeezik <mkihanya.33@gmail.com>
2025-06-16 10:24:28 -0700

    Display board with createGameboard factory function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Node prints things");
+
+function createGameboard (){
+  const gameBoard = ["_", "_", "|", "_", "|", "_", "_", "\n", "_", "_", "|", "_", 
+    "|", "_", "_", "\n", " ", " ", "|", " ", "|", " ", " "];
+
+  const displayBoard = function() {
+    return `${gameBoard.join("")}`
+  }
+
+  return {gameBoard, displayBoard}
+}
+
+gameBoard = createGameboard();
+console.log(gameBoard.displayBoard());
+
+// const Gameboard = {
+//   board: ["_", "_", "|", "_", "|", "_", "_", "\n", "_", "_", "|", "_", "|", "_", "_", "\n", " ", " ", "|", " ", "|", " ", " "],
+// }
+
+// console.log(Gameboard.board.join(""))
+
