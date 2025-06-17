1ed07e4ced399787b56e249610e3c78433163c00
weeezik <mkihanya.33@gmail.com>
2025-06-16 23:23:21 -0700

    Midst of writing method to change position value based on user input

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const GameController = (function(){
+  const changePositionValue = (board, position, value) => board[position] = value;
+  const printBoard = (board) => console.log(board);
+  return {printBoard, changePositionValue};
+})();
+
 const GameBoard = (function(){
   const board = ["_", "_", "|", "_", "|", "_", "_", "\n", "_", "_", "|", "_", 
     "|", "_", "_", "\n", " ", " ", "|", " ", "|", " ", " "];
@@ -7,7 +13,29 @@ const GameBoard = (function(){
   return {display}
 })();
 
-console.log(GameBoard.display());
+
+const Player = (function(){
+  posValueObject = {position: "", marker: ""};
+  const placeMarker = function () {
+    posValueObject = {position: 1, marker: "X"}
+    // posValueObject[position] = 1; 
+    // posValueObject[marker] = "X";
+    return posValueObject;
+  };
+
+  return {placeMarker};
+})();
+
+console.log(Player.placeMarker());
+GameController.printBoard(GameBoard.display());
+
+GameController.changePositionValue(GameBoard.display(), Player.placeMarker().position, Player.placeMarker().marker)
+
+GameController.printBoard(GameBoard.display());
+
+
+
+
 
 // Module Example (IIFEs)
 // const Formatter = (function(){
@@ -16,7 +44,6 @@ console.log(GameBoard.display());
 //   const writeToDom = (selector, message) => {
 //     document.querySelector(selector).textContent = message;
 //   };
-
 
 //   return {makeUppercase, writeToDom}
 // })();
