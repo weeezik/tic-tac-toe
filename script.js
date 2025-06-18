6e1144ec5befbd4de227d4929e70938b45675e9b
weeezik <mkihanya.33@gmail.com>
2025-06-18 10:21:06 -0700

    Change board values and active player

--- script.js
+++ script.js
@@ -1,67 +1,51 @@
-function Space() {
-  let value = "";
-
-  const addMarker = (player) => {
-    value = player.marker
-  }
-
-  const getValue = () => value;
-
-  return {addMarker, getValue}
-}
-
-const GameBoard = (function(){
-  const board = [];
-
-  // const rows = 3;
-  // const columns = 3;
-  // for (let i=0; i < rows; i++) {
-  //   board[i] = [];
-  //   for (let j=0; j < columns; j++) {
-  //     board[i] = board.push(Space());
-  //   }
-  // }
-
-  const displayBoard = () => board;
-
-  const changeSpaceValue = (positionSelected, currentPlayer) => {
-    //In the 3rd column and the first row, place an "X"
-    board[positionSelected] = currentPlayer.marker;
-    return board
-  }
-  
-  return {displayBoard, changeSpaceValue}
-})();
-
-
 function createPlayer (playerNum) {
   const name = `Player ${playerNum}`;
   const marker = playerNum === 1 ? "X" : "O";
   return {name, marker};
 }
 
+const GameBoard = (function(){
+  const board = [];
+  const displayBoard = () => board;
+  const changeSpaceValue = (currentPlayer) => {
+    board.push(currentPlayer.marker);
+
+    return board
+  }
+  
+  return {displayBoard, changeSpaceValue}
+})();
 
 const GameController = (function(){
-  const board = GameBoard.displayBoard();
-
   const player1 = createPlayer(1);
   const player2 = createPlayer(2);
   let activePlayer = player1;
-
   const switchPlayerTurn = () => activePlayer = activePlayer === player1 ? player2 : player1;
-  const getActivePlayer = () => activePlayer.name;
-
-  const turn = function(positionSelected = 2, player = getActivePlayer()) {
-    GameBoard.changeSpaceValue(positionSelected, player)
+  const getActivePlayer = () => activePlayer;
+  const turn = function(player = getActivePlayer()) {
+    GameBoard.changeSpaceValue(player)
   }
-  
 
-  return {board, switchPlayerTurn, getActivePlayer, turn}
-
+  return {switchPlayerTurn, getActivePlayer, turn}
 })();
 
-// console.log(GameController.getActivePlayer())
-console.log(GameBoard.displayBoard());
+console.log(GameBoard.displayBoard())
+console.log(GameBoard.changeSpaceValue(GameController.getActivePlayer()));
+console.log(GameBoard.displayBoard())
+GameController.switchPlayerTurn();
+console.log(GameBoard.changeSpaceValue(GameController.getActivePlayer()));
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Module Example (IIFEs)
 // const Formatter = (function(){
