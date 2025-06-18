97ee15f74276a80c1e5b91e9a44007c2862b4d9b
weeezik <mkihanya.33@gmail.com>
2025-06-17 21:37:38 -0700

    Add Space object for each space in gameboard

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
+function Space() {
+  let value = "";
+  const addMarker = (player) => {
+    value = player.marker
+  }
+  const getValue = () => value;
+
+  return {addMarker, getValue}
+}
+
 const GameBoard = (function(){
-  const board = ["_", "_", "|", "_", "|", "_", "_", "\n", "_", "_", 
-    "|", "_", "|", "_", "_", "\n", " ", " ", "|", " ", "|", " ", " "];
-  const display = () => console.log(`${board.join("")}`);
+  const board = [];
+  const rows = 3;
+  const columns = 3;
+
+  for (let i=0; i < rows; i++) {
+    board[i] = []
+    for (let j=0; j < columns; j++) {
+      board[j] = board.push(Space());
+    }
+  }
+
+  const displayBoard = () => board;
+
   const changePositionValue = (positionSelected, currentPlayer) => {
     board[positionSelected] = currentPlayer.marker;
 
     return board
   }
   
-  return {display, changePositionValue}
+  return {displayBoard, changePositionValue}
 })();
 
-const Player = (function(){
-  const createPlayer = (playerNum) => {
-    const name = `Player ${playerNum}`;
-    const marker = playerNum === 1 ? "X" : "O";
-    return {name, marker}
-  }
-  // const selectPosition = () => {}
 
-  return {createPlayer};
-})();
+function createPlayer (playerNum) {
+  const name = `Player ${playerNum}`;
+  const marker = playerNum === 1 ? "X" : "O";
+  return {name, marker};
+}
+
 
 const GameController = (function(){
   const board = GameBoard.display();
 
-  const player1 = Player.createPlayer(1)
-  const player2 = Player.createPlayer(2);
-  let activePlayer = player1
+  const player1 = createPlayer(1);
+  const player2 = createPlayer(2);
+  let activePlayer = player1;
 
   const switchPlayerTurn = () => activePlayer = activePlayer === player1 ? player2 : player1;
-  const getActivePlayer = () => activePlayer;
+  const getActivePlayer = () => activePlayer.name;
 
-  return {board, switchPlayerTurn, getActivePlayer}
+  const turn = function(positionSelected = 2, player = getActivePlayer()) {
+    GameBoard.changePositionValue(positionSelected, player)
+  }
+  
+
+  return {board, switchPlayerTurn, getActivePlayer, turn}
 
 })();
 
-GameController.board;
-GameBoard.changePositionValue(3, Player.createPlayer(1))
-GameBoard.display();
-console.log(GameController.getActivePlayer());
+console.log(GameController.getActivePlayer())
 
 // Module Example (IIFEs)
 // const Formatter = (function(){
